049349932b2f5212191a206bf996c50da6bbc5da
nakamura1111 <5040.armuakan@gmail.com>
2020-09-21 17:54:35 +0900

    パラメータ一覧機能のビュー作成

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,7 +1,7 @@
 class TargetsController < ApplicationController
 
   def index
-    
+    @targets = Target.where(user: current_user).includes(:user)
   end
 
   def new
@@ -11,10 +11,10 @@ class TargetsController < ApplicationController
   def create
     @target = Target.new(target_params)
     if @target.save
-      flash[:success] = "パラメータ登録完了"
+      flash[:success] = "能力値登録完了"
       redirect_to root_path
     else
-      flash[:error] = "パラメータ登録失敗"
+      flash[:error] = "能力値登録失敗"
       render 'new'
     end
   end
@@ -22,7 +22,16 @@ class TargetsController < ApplicationController
   private
 
   def target_params
-    params.require(:target).permit(:name, :content).merge(point: 0, user: current_user)
+    point = params[:target][:point]
+    point = 0 if point == nil       # 初期値設定
+    level, exp = level_and_exp_calc(point)
+    params.require(:target).permit(:name, :content).merge(user: current_user, point: point, level: level, exp: exp)
   end
-  
+
+  # 10expでレベルが1上がる設定になっています。(仮設定)
+  def level_and_exp_calc(point)
+    level = point/10 + 1
+    exp = point%10
+    return level, exp
+  end
 end
