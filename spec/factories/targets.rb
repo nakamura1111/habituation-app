37dc585d3d51e09689641027e8f202826d1c4e80
nakamura1111 <5040.armuakan@gmail.com>
2020-09-21 18:02:52 +0900

    Targetモデルの単体テストコードの修正

--- spec/factories/targets.rb
+++ spec/factories/targets.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     name { Faker::Lorem.paragraph_by_chars(number: 255) }
     content { Faker::Lorem.paragraph_by_chars(number: 512) }
     point { Faker::Number.between(from: 0, to: 1_000_000_000) }
+    level { Faker::Number.between(from: 0, to: 1_000_000_000) }
+    exp { Faker::Number.between(from: 0, to: 1_000_000_000) }
     association :user
   end
 end
