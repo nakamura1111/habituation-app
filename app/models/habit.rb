6144d09a5150bbbb020050e3239e416b710264a7
nakamura1111 <5040.armuakan@gmail.com>
2020-09-22 12:31:00 +0900

    Habitモデルの単体テストコード

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,14 +1,15 @@
 class Habit < ApplicationRecord
+  validates :content, length: { maximum: 500 }
   with_options presence: true do
-    validates :name
+    validates :name, length: { maximum: 50 }
     with_options numericality: { only_integer: true, allow_blank: true } do
-      validates :difficulty_grade
-      validates :achieved_or_not_binary
-      validates :achieved_days
+      validates :difficulty_grade 
+      validates :achieved_or_not_binary 
+      validates :achieved_days, numericality: { less_than: 1_000_000_000, allow_blank: true } 
     end
-    validates :is_active
   end
-
+  validates :is_active, inclusion: { in: [true, false]}
+  
   belongs_to :target
 
   # ActiveHashによるアソシエーション
