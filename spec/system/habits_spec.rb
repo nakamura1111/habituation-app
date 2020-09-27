284621e52429ea2b77b36250cc9ea423c72a2563
nakamura1111 <5040.armuakan@gmail.com>
2020-09-27 17:25:31 +0900

    結合テストコード作成

--- spec/system/habits_spec.rb
+++ spec/system/habits_spec.rb
@@ -43,3 +43,26 @@ RSpec.describe '習慣の登録機能', type: :system do
     end
   end
 end
+
+RSpec.describe '習慣の達成チェック機能', type: :system do
+  before do
+    @habit = FactoryBot.create(:habit)
+    @target = @habit.target
+  end
+  context '習慣達成の記録ができるとき' do
+    it '達成状況のクリックすると登録される' do
+      # ログインして、目標の詳細ページに遷移
+      visit_target_show_action(@target)
+      # 今日の日付のクリックして、達成状況を更新する
+      find("#achieved-check-cell-#{@habit.id}").click_link('達成したらチェック')
+      # 目標詳細表示画面に遷移していることを確認する
+      expect(current_path).to eq(target_path(@target))
+      # 達成状況が反映されていることを確認する
+      @habit.reload
+      expect(all("tr.achieved-status-row th")[6]).to have_content("〇")
+      expect(@habit.achieved_or_not_binary).to eq(1)
+      expect(@habit.achieved_days).to eq(1)
+      expect().to eq('レベルの反映忘れずに')
+    end
+  end
+end
