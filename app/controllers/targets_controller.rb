18872f20f6c11ef2373b4911f6380831a4c764aa
nakamura1111 <64011813+nakamura1111@users.noreply.github.com>
2020-09-27 21:01:40 +0900

    Revert "目標振り返り機能の実装"

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,5 +1,5 @@
-# 目標に関する機能を実装するためのコントローラー
 class TargetsController < ApplicationController
+
   def index
     @targets = Target.where(user: current_user).includes(:user)
   end
@@ -11,10 +11,10 @@ class TargetsController < ApplicationController
   def create
     @target = Target.new(target_params)
     if @target.save
-      flash[:success] = '能力値登録完了'
+      flash[:success] = "能力値登録完了"
       redirect_to root_path
     else
-      flash[:error] = '能力値登録失敗'
+      flash[:error] = "能力値登録失敗"
       render :new
     end
   end
@@ -26,7 +26,7 @@ class TargetsController < ApplicationController
     # 達成状況を配列形式に変換
     @achieved_statuses = []
     @target.habits.each do |habit|
-      @achieved_statuses << set_achieved_status(@num_days, habit.achieved_or_not_binary)
+      @achieved_statuses << set_achieved_status( @num_days, habit.achieved_or_not_binary)
     end
   end
 
@@ -34,29 +34,29 @@ class TargetsController < ApplicationController
 
   def target_params
     point = params[:target][:point]
-    point = 0 if point.nil? # 初期値設定
+    point = 0 if point == nil       # 初期値設定
     level, exp = level_and_exp_calc(point)
     params.require(:target).permit(:name, :content).merge(user: current_user, point: point, level: level, exp: exp)
   end
 
   # 10expでレベルが1上がる設定になっている。(仮設定)
   def level_and_exp_calc(point)
-    level = point / 10 + 1
-    exp = point % 10
-    [level, exp]
+    level = point/10 + 1
+    exp = point%10
+    return level, exp
   end
 
-  # 二進数データをview表示の形式に置き換える　（HabitsAchievedStatusesControllerにもう一個同じメソッドがあるので統合する必要あり）
+  # 二進数データをview表示の形式に置き換える
   def set_achieved_status(num_days, achieved_or_not_binary)
     status = []
     num_days.times do |i|
       # 新しい順に判定する
-      if ((achieved_or_not_binary >> i) & 1) == 1
-        status.push('〇')    # 新しいデータが先頭になるように格納
+      if ( ( achieved_or_not_binary >> i ) & 1 ) == 1
+        status.push("〇")    # 新しいデータが先頭になるように格納
       else
-        status.push('×')
+        status.push("×")
       end
     end
-    status
+    return status
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-# deviseで対応できないルーティングに関する例外処理を実装するためのコントローラー
 class UsersController < ApplicationController
   # deviseのルーティングエラーを解消するため、deviseとは別に作成されたコントローラです。
   def retake_registration
